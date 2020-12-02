2a01a1071b39e141ffa8a7d38baa188defebac6b
Iuliia Tokarevskaia <iuliia.tokarevskaia@socialbakers.com>
2020-12-02 07:59:32 +0100

    Проверено

--- task2/codeJS.js
+++ task2/codeJS.js
@@ -1,3 +1,5 @@
-document.querySelector('.j-btn-test').addEventListener('click', size => {
+document.querySelector('.j-btn-test').addEventListener('click', () => {
     alert(`Ширина экрана равна ${window.screen.width}, высота экрана равна ${window.screen.height}.`)
 });
+
+// Параметр size нигде не используется, поэтому лучше его не объявлять
